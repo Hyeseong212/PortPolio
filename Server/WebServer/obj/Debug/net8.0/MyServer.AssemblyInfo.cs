ace371484de80d64ec6eece0ae830a8442243aff
Hyeseong212 <86096770+Hyeseong212@users.noreply.github.com>
2024-08-07 08:45:14 +0900

    123

--- Server/WebServer/obj/Debug/net8.0/MyServer.AssemblyInfo.cs
+++ Server/WebServer/obj/Debug/net8.0/MyServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e450bdb3e5da477cf79a38236213332b14498c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11bf305b1741a3c692b2ae2228f9dc687748d734")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
